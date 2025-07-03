47b36bff7254473d4e086c6115822aaf9488049f
Mahmoud-abu-zaid <v.i.p.omlat@gmail.com>
2025-07-04 00:15:27 +0300

    Add:Destructuring Assignment in course tabs

--- src/components/pages/CourseTabs.tsx
+++ src/components/pages/CourseTabs.tsx
@@ -1,20 +1,32 @@
 "use client";
 
-import { useState } from "react";
-import ActiveCurriculum from "../ui/ActiveCurriculum";
+import FAQs from "../ui/FAQs";
 import Image from "next/image";
+import { useState } from "react";
+import Rating from "../ui/Rating";
+import { CiStar } from "react-icons/ci";
+import { useForm } from "react-hook-form";
 import { IoIosTime } from "react-icons/io";
 import { MdFileCopy } from "react-icons/md";
+import ActiveCurriculum from "../ui/ActiveCurriculum";
 import { FaFacebookF, FaInstagram, FaPinterestP, FaStar, FaXTwitter, FaYoutube } from "react-icons/fa6";
-import FAQs from "../ui/FAQs";
-import { CiStar } from "react-icons/ci";
-import Rating from "../ui/Rating";
 
+interface DateForComment {
+  name: string;
+  email: string;
+  comment: string;
+}
 const tabs = ["Overview", "Curriculum", "Instructor", "FAQs", "Reviews"];
 
 export default function CourseTabs() {
   const [activeTab, setActiveTab] = useState("Overview");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<DateForComment>();
 
+  function onSubmit() {}
   return (
     <div className="">
       <div className="flex justify-around items-center w-[100%] px-2">
@@ -153,18 +165,71 @@ export default function CourseTabs() {
         <div className="lg:w-[51%] w-full px-3">
           <h3 className="text-2xl">Leave a comment</h3>
           <p>Your email address will not be published. Required fields are marked *</p>
-          <form className="flex flex-col gap-2">
+          <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2">
             <div className="flex gap-2 mt-2">
-              <input type="text" placeholder="Name*" className=" border-[1px] border-gray-400 px-1 py-1 rounded-md w-full" />
-              <input type="email" placeholder="Email*" className=" border-[1px] border-gray-400 px-1 py-1 rounded-md w-full" />
+              <div className="w-full">
+                <input
+                  type="text"
+                  placeholder="Name*"
+                  {...register("name", {
+                    required: "Enter your name",
+                    pattern: {
+                      value: /^[A-Za-zÀ-ÖØ-öø-ÿ' -]{2,20}$/,
+                      message: "Name must be 2-15 letters",
+                    },
+                    maxLength: {
+                      value: 20,
+                      message: "The maximum character limit is 20 char",
+                    },
+                    minLength: {
+                      value: 2,
+                      message: "The minimum number of characters is 2",
+                    },
+                  })}
+                  className=" border-[1px] block border-gray-400 px-1 py-1 rounded-md w-full"
+                />
+                <p className="text-red-500 text-sm">{typeof errors?.name?.message === "string" ? errors?.name?.message : ""}</p>
+              </div>
+              <div className="w-full">
+                <input
+                  type="email"
+                  {...register("email", {
+                    required: "Enter your email",
+                    pattern: {
+                      value: /^[aA-zZ0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/i,
+                      message: "Invalid email format",
+                    },
+                  })}
+                  placeholder="Email*"
+                  className=" border-[1px] border-gray-400 px-1 py-1 rounded-md w-full"
+                />
+                <p className="text-red-500 text-sm">{typeof errors?.email?.message === "string" ? errors?.email?.message : ""}</p>
+              </div>
             </div>
             <div>
-              <textarea placeholder="Comment" className=" border-[1px] border-gray-400 px-1 py-1 my-2 rounded-md w-full"></textarea>
+              <textarea
+                placeholder="Comment"
+                {...register("comment", {
+                  required: "Enter your message",
+                  maxLength: {
+                    value: 100,
+                    message: "The maximum character limit is 100 char",
+                  },
+                  minLength: {
+                    value: 20,
+                    message: "The minimum number of characters is 20",
+                  },
+                })}
+                className=" border-[1px] border-gray-400 px-1 py-1 rounded-md w-full"
+              ></textarea>
+              <p className="text-red-500 text-sm">{typeof errors?.comment?.message === "string" ? errors?.comment?.message : ""}</p>
               <div className="flex items-center gap-1">
                 <input type="checkbox" />
                 <p>Save my name, email in this brower for the next time I comment</p>
               </div>
-              <button className="py-2 px-3 bg-amber-500 text-white mt-3 rounded-3xl cursor-pointer">Posts comment</button>
+              <button type="submit" className="py-2 px-3 bg-amber-500 text-white mt-3 rounded-3xl cursor-pointer">
+                Posts comment
+              </button>
             </div>
           </form>
         </div>
