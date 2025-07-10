9d1a6673a525f4d3c2e9aaf3ae5df13335c56c79
Mahmoud-abu-zaid <v.i.p.omlat@gmail.com>
2025-07-10 10:18:57 +0300

    Add:File json allCourses

--- src/app/AllCourses/page.tsx
+++ src/app/AllCourses/page.tsx
@@ -1,7 +1,38 @@
-export default function AllCourses() {
+import { DataCorurses } from "@/types/typeCourses";
+
+export default async function AllCourses() {
+  const response =await fetch("http://localhost:3000/json/allCourses.json", {
+    cache: "no-store",
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch featured courses");
+  }
+
+  const data: DataCorurses[] = await response.json();
+
   return (
     <>
-      <p>page Courses</p>
+      <div>
+        <div className="flex justify-between items-center py-5">
+          <div>
+            <h3 className="text-xl">All Courses</h3>
+          </div>
+          <div>
+            <input type="text" className=" border-2" />
+          </div>
+        </div>
+        <div>
+          <div>
+            {data.map((course: DataCorurses) => (
+              <div key={course.id} className="border-2 p-5 mb-5 rounded-lg">
+                {course.by}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div></div>
     </>
   );
 }
