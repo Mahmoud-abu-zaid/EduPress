fcbed8cc03d99d169e002bc3caf8149c26e306ca
Mahmoud-abu-zaid <v.i.p.omlat@gmail.com>
2025-08-24 07:39:02 +0300

    Add:page Coming Soon

---
+++ src/app/Premium/page.tsx
@@ -0,0 +1,17 @@
+export default function Premium() {
+  return (
+    <>
+      <div className="container mx-auto px-4 py-8 min-h-[70Vh]">
+        <div>
+          <h1 className="md:text-4xl font-bold mb-4 sm:text-2xl text-xl text-color-black">LearnPress Page</h1>
+          <p className="text-lg text-color-black">This is the LearnPress page content.</p>
+        </div>
+        <div className="text-center py-20">
+          <p className="text-7xl py-3 text-color-black">🚧</p>
+          <h2 className="sm:text-4xl text-3xl py-2 text-color-black">Coming Soon</h2>
+          <p className="sm:text-2xl text-lg text-color-black">This page is under construction. Check back soon!</p>
+        </div>
+      </div>
+    </>
+  );
+}
